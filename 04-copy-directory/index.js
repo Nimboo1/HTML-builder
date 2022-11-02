18023c61d2c832d1828aa0ec6f49b4caf7ed9346
Maslov Vladislav <qwertyunivese12@yandex.ru>
2022-11-02 13:44:40 +0300

    feat: resolve task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const fs = require('fs');
+const path = require("path");
+
+let folderPath = path.join(__dirname, "files");
+let destination = path.join(__dirname, "files-copy");
+
+copyDir(folderPath, destination);
+
+function copyDir(folder, dest) {
+    fs.mkdir(dest, { recursive: true }, (err) => { if (err) throw err; });
+
+    fs.readdir(
+        folder, 
+        { withFileTypes: true },
+        (err, files) => {
+            if (err)
+                console.log(err);
+            else {
+                files.forEach(file => {
+                    if (file.isFile()) {
+                        let filePath = path.join(folderPath, file.name.toString());
+                        let destPath = path.join(dest, file.name.toString());
+                        
+                        fs.copyFile(filePath, destPath, (err) => {
+                            if (err) {
+                              console.log("Error Found:", err);
+                            }   
+                        });
+                    }
+                });
+            }
+        }
+    );
+}
 
