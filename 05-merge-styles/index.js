99649c9fc474f6bc1808e9d694f289e78e9793b3
Maslov Vladislav <qwertyunivese12@yandex.ru>
2022-11-03 12:03:19 +0300

    feat: resolve task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const path = require("path");
+
+let sourcePath = path.join(__dirname, "styles");
+let dest = path.join(__dirname, "project-dist/bundle.css");
+
+fs.open(dest, "w+", (err) => {
+    if(err) throw err;
+});
+const output = fs.createWriteStream(dest);
+
+fs.readdir(
+    sourcePath, 
+    { withFileTypes: true },
+    (err, files) => {
+        if (err)
+            console.log(err);
+        else {
+            files.forEach(file => {
+                if (file.isFile() && file.name.toString().split(".")[1] == "css") {
+                    let filePath = path.join(sourcePath, file.name.toString());
+                    
+                    const input = fs.createReadStream(filePath, "utf-8");
+                    input.pipe(output);
+                }
+            });
+        }
+    }
+);
+
